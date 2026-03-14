9824fb74ea18543dcbfec0a84e85989f4283a9df
dilbermt <88415739+dilbermt@users.noreply.github.com>
2026-03-14 16:11:29 +0530

    changed the pricing

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const CONTACT_DETAILS = {
-  phone: "+919876543210",
-  whatsappNumber: "919876543210", // Used for wa.me links
+  phone: "+919747119308",
+  whatsappNumber: "919747119308", // Used for wa.me links
   instagramUrl: "https://www.instagram.com/azarperfumes_/"
 };
